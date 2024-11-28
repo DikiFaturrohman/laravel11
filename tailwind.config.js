20ee20a737006878253083d0d8c8c1f3a345d6a7
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2024-11-28 15:52:45 +0700

    Model Factory

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+const defaultTheme = require('tailwindcss/defaultTheme')
+export default {
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+        },
+      },
+    },
+    plugins: [],
+  }
